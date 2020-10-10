8bee996175be82a3dc59a7dcfccd5fb7f7182948
Administrator <Administrator@FZ4T6RD9Y4XBY0A>
2020-10-10 19:35:42 +0700

    m

---
+++ HelloSpringMVCNumberValidation2/src/main/java/com/buiminhtien/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.buiminhtien.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.buiminhtien.employee.Employee;
+
+@Controller
+public class EmployeeController {
+@RequestMapping("/hello")
+public String display(Model m) {
+	m.addAttribute("emp", new Employee());
+	return "viewpage";
+}
+@RequestMapping("/helloagain")
+public String submitForm( @Validated @ModelAttribute("emp") Employee e, BindingResult br) {
+	if(br.hasErrors()) {
+		return "viewpage";
+	}
+	else {
+		return "final";
+	}
+}
+}
+
---
+++ HelloSpringMVCNumberValidation2/src/main/webapp/WEB-INF/views/viewpage.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    	<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<style>
+.error{color:red}
+</style>
+</head>
+<body>
+<form:form action="helloagain" modelAttribute="emp">
+Username: <form:input path="name"/>
+<form:errors path="name" cssClass="error"/><br></br>
+Password: <form:password path="pass"/>
+<form:errors path="pass" cssClass="error"/><br></br>
+Age: <form:input path="age"/>
+<form:errors path="age" cssClass="error"/><br></br>
+<input type="submit" value="submit">
+</form:form>
+</body>
+</html>
