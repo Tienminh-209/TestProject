d80af2608b859ec93e1252aa6d046f793df8bd87
Administrator <Administrator@FZ4T6RD9Y4XBY0A>
2020-10-10 19:38:24 +0700

    m3

---
+++ HelloSpringMVC_JDBC/src/main/java/com/buiminhtien/service/CustomerService.java
@@ -0,0 +1,36 @@
+package com.buiminhtien.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.buiminhtien.dao.CustomerDAO;
+import com.buiminhtien.entities.Customer;
+@Service
+@Transactional
+public class CustomerService {
+  @Autowired
+  private CustomerDAO customerDAO;
+  
+  public List<Customer> findAll() {
+    return customerDAO.findAll();
+  }
+  public Customer findById(int id) {
+    return customerDAO.findById(id);
+  }
+  
+  public void save(Customer customer){
+    // validate business
+    customerDAO.save(customer);
+  }
+  public void update(Customer customer){
+    // validate business
+    customerDAO.update(customer);
+  }
+  
+  public void delete(int id){
+    // validate business
+	  customerDAO.delete(id);
+  }
+}
